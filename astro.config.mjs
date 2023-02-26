7e69cb18c76022484a314fc0d6ecf5651f03767b
Kazuto Kusama <jacopen@gmail.com>
2023-02-26 22:45:37 +0900

    Use Notion as headless CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,35 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-//import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import { CUSTOM_DOMAIN, BASE_PATH } from './src/server-constants';
+import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
+import PublicNotionCopier from './src/integrations/public-notion-copier';
+
+const getSite = function() {
+    if (!process.env.CF_PAGES) {
+        return new URL(BASE_PATH, 'http://localhost:3000').toString();
+    }
+
+    if (process.env.CF_PAGES_BRANCH !== 'main') {
+        return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
+    }
+
+    if (CUSTOM_DOMAIN) {
+        return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
+    }
+
+    return new URL(
+        BASE_PATH,
+        `https://${new URL(process.env.CF_PAGES_URL).host
+      .split('.')
+      .slice(1)
+      .join('.')}`
+    ).toString();
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-} // output: "server",
-// adapter: vercel()
-);
+    site: getSite(),
+    base: BASE_PATH,
+    integrations: [PublicNotionCopier(), tailwind()],
+    //integrations: [FeaturedImageDownloader(), PublicNotionCopier(), tailwind()],
+});
