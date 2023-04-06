7a103ca6934b943c3f8c3e838f6a1bf536cdf0f8
Kazuto Kusama <jacopen@gmail.com>
2023-04-06 22:39:49 +0900

    Add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,43 @@
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from 'astro/config';
-import { CUSTOM_DOMAIN, BASE_PATH } from './src/server-constants';
-import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
-import PublicNotionCopier from './src/integrations/public-notion-copier';
+import { defineConfig } from "astro/config";
+import { CUSTOM_DOMAIN, BASE_PATH } from "./src/server-constants";
+import FeaturedImageDownloader from "./src/integrations/featured-image-downloader";
+import PublicNotionCopier from "./src/integrations/public-notion-copier";
+import partytown from "@astrojs/partytown";
 
-const getSite = function() {
-    if (!process.env.CF_PAGES) {
-        return new URL(BASE_PATH, 'http://localhost:3000').toString();
-    }
+const getSite = function () {
+  if (!process.env.CF_PAGES) {
+    return new URL(BASE_PATH, "http://localhost:3000").toString();
+  }
 
-    if (process.env.CF_PAGES_BRANCH !== 'main') {
-        return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
-    }
+  if (process.env.CF_PAGES_BRANCH !== "main") {
+    return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
+  }
 
-    if (CUSTOM_DOMAIN) {
-        return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
-    }
+  if (CUSTOM_DOMAIN) {
+    return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
+  }
 
-    return new URL(
-        BASE_PATH,
-        `https://${new URL(process.env.CF_PAGES_URL).host
-      .split('.')
+  return new URL(
+    BASE_PATH,
+    `https://${new URL(process.env.CF_PAGES_URL).host
+      .split(".")
       .slice(1)
-      .join('.')}`
-    ).toString();
+      .join(".")}`
+  ).toString();
 };
 
-// https://astro.build/config
 export default defineConfig({
-    site: getSite(),
-    base: BASE_PATH,
-    integrations: [PublicNotionCopier(), tailwind()],
-    //integrations: [FeaturedImageDownloader(), PublicNotionCopier(), tailwind()],
+  site: getSite(),
+  base: BASE_PATH,
+  integrations: [
+    PublicNotionCopier(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+  //integrations: [FeaturedImageDownloader(), PublicNotionCopier(), tailwind()],
 });
