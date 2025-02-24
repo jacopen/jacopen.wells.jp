cfeb51db2c60a05709c7d0eef14f342776612f9b
Kazuto Kusama <jacopen@gmail.com>
2025-02-24 16:44:13 +0900

    コンテンツを追加

---
+++ src/data/profile-content.ts
@@ -0,0 +1,132 @@
+export const blogPosts = [
+  {
+    url: "https://www.pagerduty.co.jp/blog/11-event-driven-automation-tags/",
+    title: "DevOpsチームのための自動化レシピ集：すぐに使える11の実践例",
+  },
+  {
+    url: "https://www.pagerduty.co.jp/blog/pagerduty-innovation-2025/",
+    title:
+      "2025年の幕開け−より高度な運用管理を実現する、PagerDutyのイノベーション",
+  },
+  {
+    url: "https://www.pagerduty.co.jp/blog/itil-event-management/",
+    title:
+      "ITILにおけるイベント管理～効率化と迅速なインシデント対応を実現するフレームワーク",
+  },
+  {
+    url: "https://www.pagerduty.co.jp/blog/incident-communication-best-practices/",
+    title: "障害時に適切なコミュニケーションを実現するベストプラクティス",
+  },
+  {
+    url: "https://www.pagerduty.co.jp/blog/deploy-pipeline/",
+    title: "CI／CDパイプライン構築に重要なデプロイパイプラインとは？",
+  },
+  {
+    url: "https://qiita.com/jacopen/items/76a31438e33d3791302a",
+    title: "PagerDutyに入社して3週間が経ったので、色々紹介してみる",
+  },
+  {
+    url: "https://qiita.com/jacopen/items/75c0c16de744582d7b8d",
+    title: "PagerDutyに入ってから1年とちょっと経った話",
+  },
+];
+
+export const books = [
+  {
+    url: "https://book.impress.co.jp/books/1121101117",
+    title: "入門Terraform クラウド時代のインフラ統合管理",
+  },
+  {
+    url: "https://techbookfest.org/product/h21RurMmFNU2Aee7Kg8Hh4?productVariantID=k2eLkqZic28carLXArVy6M",
+    title: "ちいさく始めるプラットフォームエンジニアリング",
+  },
+  {
+    url: "https://nextpublishing.jp/book/17823.html",
+    title: "Kubernetes Secret管理入門 HashiCorp Vaultで実現するセキュアな運用",
+  },
+  {
+    url: "https://techbookfest.org/product/qunTLHG5hLbL91bBX9dqDU?productVariantID=diV811bQsBeU5YfWhtGym0",
+    title: "Real World Platform Engineering: 現場の知恵とノウハウ",
+  },
+];
+
+export const presentations = [
+  {
+    url: "https://speakerdeck.com/jacopen/torasiyuanimaruninarou-kai-fa-zhe-dakarakosodekiru-an-ding-sitasabisuzuo-rinomi-jue",
+    title:
+      "トラシューアニマルになろう ～開発者だからこそできる、安定したサービス作りの秘訣～",
+    event: "",
+  },
+  {
+    url: "https://speakerdeck.com/jacopen/anatanoxing-wei-haxin-lai-xing-soretomosheng-chan-xing-sretositenokiyarianinao-muminasamanichuan-etaixuan-ze-zhi",
+    title:
+      "あなたの興味は信頼性？それとも生産性？ SREとしてのキャリアに悩むみなさまに伝えたい選択肢",
+    event: "",
+  },
+  {
+    url: "https://speakerdeck.com/jacopen/jian-wei-itarakenohosutomotemu-hontoniyi-li-turehiyuhakouta",
+    title: "間違いだらけのポストモーテム – ホントに役立つレビューはこうだ！",
+    event: "",
+  },
+  {
+    url: "https://speakerdeck.com/jacopen/ai-x-insidentoguan-li-dekuo-gerusabisuonasitupu",
+    title: "AI x インシデント管理で拡げるサービスオーナーシップ",
+    event: "",
+  },
+];
+export const contributions = [
+  {
+    url: "https://thinkit.co.jp/article/14044",
+    title:
+      "Kubernetes、PaaS、Serverlessのどれを選ぶのか？ 機能比較と使い分けのポイント",
+  },
+  {
+    url: "https://enterprisezine.jp/article/corner/621",
+    title: "システム障害管理の効率化──AIOpsへの道筋一覧",
+  },
+  {
+    url: "https://codezine.jp/article/corner/990",
+    title:
+      "プラットフォームづくりを成功に導く！開発者のための「Platform Engineering」入門一覧",
+  },
+  {
+    url: "https://codezine.jp/article/detail/19221",
+    title:
+      "サービス危機を解決へと導く「インシデントコマンダー」の役割とは？ 組織で取り組むインシデント対応と戦術",
+  },
+  {
+    url: "https://atmarkit.itmedia.co.jp/ait/series/16443/",
+    title: "草間一人×青山真也 クラウドネイティブ対談",
+  },
+  {
+    url: "https://atmarkit.itmedia.co.jp/ait/articles/2302/21/news006.html",
+    title:
+      "草間氏が語るクラウドネイティブ推進のための視点　「リリース間隔を短くしたいなら会議の削減も大切」の理由とは？",
+  },
+  {
+    url: "https://techtarget.itmedia.co.jp/tt/series/2931/",
+    title: "IT部門もアジャイル、DevOpsで行こう",
+  },
+  {
+    url: "https://atmarkit.itmedia.co.jp/ait/series/28923/",
+    title: "DXに悩むITマネジャーにささげる！ クラウドネイティブ講座",
+  },
+  {
+    url: "https://atmarkit.itmedia.co.jp/ait/articles/2006/01/news033.html",
+    title: "「Kubernetes-native」へと舵を切るCloud Foundry、その理由と展望は？",
+  },
+  {
+    url: "https://techtarget.itmedia.co.jp/tt/news/1710/11/news01.html",
+    title:
+      "クラウド移行後に取り組みたい、「コンテナ」「アプリプラットフォーム」「サーバレス」って何？",
+  },
+  {
+    url: "https://thinkit.co.jp/article/18294",
+    title: "最高のオンラインカンファレンスを目指して 〜CloudNative Daysの挑戦",
+  },
+  {
+    url: "https://thinkit.co.jp/article/22149",
+    title:
+      "CI/CD Conference 2023から、HashiCorpのエンジニアがCI/CDにおけるシークレット管理のコツを解説",
+  },
+];
